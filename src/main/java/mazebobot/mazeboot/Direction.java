a3d7f5d4f8fc01f7d2b6753239bb790a8d664d6f
AditheD <adithedas20@gmail.com>
2026-09-21 18:26:57 -0400

    Implement direction enum

--- src/main/java/mazebobot/mazeboot/Direction.java
+++ src/main/java/mazebobot/mazeboot/Direction.java
@@ -1,4 +1,30 @@
 package mazebobot.mazeboot;
 
 public enum Direction {
+    UP(0, -1, -90),
+    DOWN(0, 1, 90),
+    LEFT(-1, 0, 180),
+    RIGHT(1, 0, 0);
+
+    private final int changeX;
+    private final int changeY;
+    private final double angle;
+
+    Direction(int changeX, int changeY, double angle) {
+        this.changeX = changeX;
+        this.changeY = changeY;
+        this.angle = angle;
+    }
+
+    public int getChangeX() {
+        return changeX;
+    }
+
+    public int getChangeY() {
+        return changeY;
+    }
+
+    public double getAngle() {
+        return angle;
+    }
 }
