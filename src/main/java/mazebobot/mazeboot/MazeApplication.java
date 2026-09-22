284d3f55a7abe60ebdbe59fe55fbe76d58d00df0
Christian <cep812@gmail.com>
2026-09-22 16:38:05 -0400

    Add basic JavaFX window setup to MazeApplication

--- src/main/java/mazebobot/mazeboot/MazeApplication.java
+++ src/main/java/mazebobot/mazeboot/MazeApplication.java
@@ -1,6 +1,23 @@
 package mazebobot.mazeboot;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
-public class MazeApplication  {
+public class MazeApplication extends Application {
 
+    @Override
+    public void start(Stage stage) {
+        Pane root = new Pane();
+        Scene scene = new Scene(root, 600, 600);
+
+        stage.setScene(scene);
+        stage.setTitle("Maze Robot");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
